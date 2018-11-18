b6af6c69f0003be9708ea3c9c516ad0964af2d36
buddyest <44507465+buddyest@users.noreply.github.com>
2018-11-18 19:24:21 +0530

    Added client slider , Testimonial slider

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,45 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+  $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+  $('.testimonial').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+
 });
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
